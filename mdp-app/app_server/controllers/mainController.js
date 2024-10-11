e280060ae01c9f653e91546e362cd44e2d4c93ef
CristianoRico <carvalhocristianorico@gmail.com>
2024-10-11 11:20:31 +0700

    express application

---
+++ mdp-app/app_server/controllers/mainController.js
@@ -0,0 +1,24 @@
+const index = (req,res) => {
+  const berita = [
+      {
+          judul: 'Berita 1',
+          isi: 'Isi Berita 1'
+      },
+      {
+          judul: 'Berita 2',
+          isi: 'Isi Berita 2'
+      },
+  ];
+
+  res.render('index', {title : 'Halaman Home', berita , layout : 'main'});
+};
+const about = (req,res) => {
+    res.render('about', {title : 'About Us', layout : 'main'});
+};
+
+const contact = (req,res) => {
+    res.render('contact', {title : 'Contact Us', layout : 'main'});
+};
+
+
+module.exports = {index, about, contact}
