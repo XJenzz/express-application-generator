70316d2aa50754b49a0e0f8a978367e87e27fe3d
CristianoRico <carvalhocristianorico@gmail.com>
2024-10-25 11:19:25 +0700

    API GET & POST PRODI

---
+++ mdp-app/app_api/controllers/prodiController.js
@@ -0,0 +1,111 @@
+// Mengimpor model prodi untuk berinteraksi dengan koleksi fakultas di MongoDB
+const Prodi = require("../models/prodi");
+
+// Mendapatkan semua data prodi
+const getAllProdi = async (req, res) => {
+    try {
+        // Mengambil semua prodi dari database
+        // const prodi = await Prodi.find();
+
+        // Mengambil semua prodi dari database dan populate data fakultas berdasarkan fakultas_id
+        const prodi = await Prodi.find().populate("fakultas_id","nama singkatan");// Mengambil filed 'nama' dari Fakultas
+        // Mengirimkan respons dengan status 200 dan data fakultas
+        res.status(200).json(prodi);
+    } catch (err) {
+        // Mengirimkan respons dengan status 500 jika terjadi kesalahan
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Mendapatkan satu prodi berdasarkan ID
+const getProdiById = async (req, res) => {
+    try {
+        // Mencari prodi berdasarkan ID yang diberikan di parameter
+        const prodi = await Prodi.findById(req.params.id);
+        // Jika prodi tidak ditemukan, kirimkan respons 404
+        if (!prodi) return res.status(404).json({ message: "Prodi not found" });
+        // Mengirimkan respons dengan status 200 dan data prodi
+        res.status(200).json(prodi);
+    } catch (err) {
+        // Mengirimkan respons dengan status 500 jika terjadi kesalahan
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Membuat prodi baru
+const createProdi = async (req, res) => {
+    // Membuat instance prodi baru dari data yang diterima
+    const prodi = new Prodi({
+        nama: req.body.nama,
+        singkatan: req.body.singkatan,
+        fakultas_id: req.body.fakultas_id,
+    });
+
+    try {
+        // Menyimpan prodi baru ke database
+        const newProdi = await prodi.save();
+        // Mengirimkan respons dengan status 201 dan data prodi baru
+        res.status(201).json(newProdi);
+    } catch (err) {
+        // Mengirimkan respons dengan status 400 jika ada kesalahan saat menyimpan
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Memperbarui data prodi
+const updateProdi = async (req, res) => {
+    try {
+        // Mencari prodi berdasarkan ID yang diberikan di parameter
+        const prodi = await Prodi.findById(req.params.id);
+        // Jika prodi tidak ditemukan, kirimkan respons 404
+        if (!prodi) return res.status(404).json({ message: "Prodi not found" });
+        
+        // Memperbarui nama prodi jika ada di request body
+        if (req.body.nama != null) {
+            prodi.nama = req.body.nama;
+        }
+        // Memperbarui singkatan prodi jika ada di request body
+        if (req.body.singkatan != null) {
+            prodi.singkatan = req.body.singkatan;
+        }
+        // Memperbarui fakultas_id jika ada di request body
+        if (req.body.fakultas_id != null) {
+            prodi.fakultas_id = req.body.fakultas_id;
+        }
+
+        // Menyimpan perubahan ke database
+        const updateProdi = await prodi.save();
+        // Mengirimkan respons dengan status 200 dan data fakultas yang diperbarui
+        res.status(200).json(updateProdi);
+    } catch (err) {
+        // Mengirimkan respons dengan status 400 jika ada kesalahan saat memperbarui
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Menghapus prodi berdasarkan ID
+const deleteProdi = async (req, res) => {
+    try {
+        // Mencari prodi berdasarkan ID yang diberikan di parameter
+        const prodi = await Prodi.findById(req.params.id);
+        // Jika prodi tidak ditemukan, kirimkan respons 404
+        if (!prodi) return res.status(404).json({ message: "Prodi not found" });
+        
+        // Menghapus prodi dari database
+        await prodi.deleteOne();
+        // Mengirimkan respons dengan status 200 dan pesan penghapusan
+        res.status(200).json({ message: "Prodi deleted" });
+    } catch (err) {
+        // Mengirimkan respons dengan status 500 jika terjadi kesalahan
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Mengeksport fungsi-fungsi kontroler agar dapat digunakan di file lain
+module.exports = {
+    getAllProdi,
+    createProdi,
+    getProdiById,
+    updateProdi,
+    deleteProdi,
+};
---
+++ mdp-app/app_api/models/prodi.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');// Mengimpor Modul mongoose untuk mengelola skema dan model
+
+const prodiSchema = new mongoose.Schema({// Definisikan skema untuk fakultas
+    nama: {// Field untuk nama fakultas
+        type: String, // Tipe data string
+        required: true, // Field ini wajib diisi
+        trim: true, // Menghapus spasi di awal dan akhir string
+    },
+    singkatan: {// Field untuk singkatan fakultas
+        type: String, // Tipe data string
+        required: true, // Field ini wajib diisi
+        trim: true, // Menghapus spasi di awal dan akhir string
+    },
+    fakultas_id: {// Field untuk singkatan fakultas
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Fakultas", // Field ini wajib diisi
+        required: true, // Menghapus spasi di awal dan akhir string
+    },
+    createdAt: {// Field untuk menyimpan tanggal pembuatan data fakultas
+        type: Date, // Tipe data tanggal
+        default: Date.now, // Default adalah tanggal dan waktu saat ini
+    },
+});
+
+// Buat model Fakultas dari skema yang telah didefinisikan
+const Prodi = mongoose.model("Prodi", prodiSchema);
+
+// Mengekspor model Fakultas agar dapat digunakan di file lain
+module.exports = Prodi;
