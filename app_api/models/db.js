afa4bc4d01086c1c81a73185b801328815df07a1
Bryan Penuntun <cyberjrs.p@gmail.com>
2024-11-28 18:04:34 +0700

    mongo

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://bryanp:bryanp@cluster0.phlqa.mongodb.net/mdpsi5a?retryWrites=true&w=majority&appName=Cluster0"
+            // "mongodb+srv://bryanp:bryanp@cluster0.phlqa.mongodb.net/mdpsi5a?retryWrites=true&w=majority&appName=Cluster0"
             // "mongodb://localhost:27017/mdpsi5a"
+            "mongodb://bryanp:bryanp@cluster0-shard-00-00.phlqa.mongodb.net:27017,cluster0-shard-00-01.phlqa.mongodb.net:27017,cluster0-shard-00-02.phlqa.mongodb.net:27017/mdpsi5a?ssl=true&replicaSet=atlas-hwgv4g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
         );
 
         
